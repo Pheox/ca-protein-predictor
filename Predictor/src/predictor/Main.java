dffc3c2e8b491ab8d896663fe3730d782f80842d
vlado <vladimirbrigant@hotmail.com>
2012-09-29 23:58:03 -0400

    Import predictor.ga classes.

--- Predictor/src/predictor/Main.java
+++ Predictor/src/predictor/Main.java
@@ -1,10 +1,20 @@
+/**
+*   GeneticAlgorithm.java
+*
+*   Copyright (c) 2012 Vladimír Brigant
+*   This software is distributed under the terms of the GNU General Public License.
+*/
+
 package predictor;
 
+import predictor.ga.*;
+
 /**
- * Hello
+ * Just for testing, use UnitTesting instead!
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Hello world");
     }
 }
