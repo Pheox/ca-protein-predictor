bdff55ef0fbf3740ec0b84316ecef48dd3f25ed6
vlado <vladimirbrigant@hotmail.com>
2013-03-14 18:29:36 +0100

    updated build.xml by prev. version

--- Predictor/src/predictor/Configuration.java
+++
@@ -1,8 +0,0 @@
-/**
-*   Configuration.java
-*
-*   Copyright (c) 2012 Vladimír Brigant
-*   This software is distributed under the terms of the GNU General Public License.
-*/
-
-package predictor;
