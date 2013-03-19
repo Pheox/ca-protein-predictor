0689d67dc7a73295ed02667c7f812e2074c2fd1e
vlado <vladimirbrigant@hotmail.com>
2013-03-19 20:54:46 +0100

    tests for accuracy measures q3 and sov

---
+++ CASSP/src/cassp/tests/TestData.java
@@ -0,0 +1,55 @@
+/**
+*   TestData.java
+*
+*   Copyright (c) 2013 Vladimir Brigant
+*   This software is distributed under the terms of the GNU General Public License.
+*/
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+import static org.junit.Assert.assertEquals;
+
+import cassp.data.Data;
+import cassp.data.DataItem;
+
+
+
+public class TestData extends TestCase {
+
+    public static Test suite() {
+        return new TestSuite(TestData.class);
+    }
+
+    public static void main(String[] args) {
+        TestRunner.run(suite());
+    }
+
+    protected Data simpleData;
+    protected Data complexData;
+
+
+    public void setUp() {
+        this.simpleData = new Data();
+        this.simpleData.add(new DataItem(
+            "AAAAAAAAAAAAAAAAAAA",
+            "CCHHHCCCCCHHHHHHCCC",
+            "CCCCCCCHHHHCCCHHCCC"));
+    }
+
+    public void tearDown() {
+        this.simpleData = null;
+    }
+
+    public void testQ3() {
+        System.out.println(this.simpleData.q3());
+        assertEquals(52.6315, this.simpleData.q3(), 0.001);
+    }
+
+    public void testSOV(){
+        System.out.println(this.simpleData.sov());
+        assertEquals(40.7613, this.simpleData.sov(), 0.001);
+    }
+}
