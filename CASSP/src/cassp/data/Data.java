329315176da7306b6569a6c4d0902e35b2137407
vlado <vladimirbrigant@hotmail.com>
2013-03-19 20:56:02 +0100

    sov implemented #7

--- CASSP/src/cassp/data/Data.java
+++ CASSP/src/cassp/data/Data.java
@@ -11,6 +11,7 @@ package cassp.data;
 import java.util.*;
 import java.io.*;
 import java.util.regex.*;
+import java.lang.Math;
 
 import org.apache.log4j.*;
 
@@ -22,44 +23,56 @@ public class Data {
 
     static Logger logger = Logger.getLogger(Data.class);
 
-    public ArrayList<DataItem> data;
-    public HashMap<Character, AminoAcid> amino_acids;
+    private ArrayList<DataItem> data;
+    private HashMap<Character, AminoAcid> aminoAcids;
 
 
+    /**
+    * Constructor 1 - initializes structures
+    */
     public Data(){
         this.data = new ArrayList<DataItem>();
-        this.amino_acids = new HashMap<Character, AminoAcid>();
+        this.aminoAcids = new HashMap<Character, AminoAcid>();
     }
 
-    public Data(String file_path){
+    /**
+    * Constructor 2 - initializes strucures + loads data
+    * @param filePath path to data file
+    */
+    public Data(String filePath){
         this.data = new ArrayList<DataItem>();
-        this.amino_acids = new HashMap<Character, AminoAcid>();
-        this.loadData(file_path);
+        this.aminoAcids = new HashMap<Character, AminoAcid>();
+        this.loadData(filePath);
     }
 
 
-    public ArrayList<DataItem> getData(){
-        return this.data;
-    }
-
-
-    public void loadData(String file_path){
+    /**
+    * Loads data from data file in format:
+    * aaSeq1
+    * sspSeq1
+    * \n
+    * aaSeq2
+    * ...
+    *
+    * @param filePath path to data file
+    */
+    public void loadData(String filePath){
 
         try{
-            FileInputStream fstream = new FileInputStream(file_path);
+            FileInputStream fstream = new FileInputStream(filePath);
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-            String aa_seq;
-            String ssp_seq;
+            String aaSeq;
+            String sspSeq;
 
-            while ((aa_seq = br.readLine()) != null)   {
-                ssp_seq = br.readLine();
+            while ((aaSeq = br.readLine()) != null)   {
+                sspSeq = br.readLine();
                 br.readLine();
 
                 DataItem di = new DataItem();
-                di.aa_seq = aa_seq;
-                di.ssp_seq = ssp_seq;
+                di.setAaSeq(aaSeq);
+                di.setSspSeq(sspSeq);
                 this.add(di);
             }
 
@@ -71,9 +84,11 @@ public class Data {
         logger.info("dataset length: " + this.data.size());
     }
 
-    public void computeChouFasman(){
-        // TODO
 
+    /**
+    * TODO
+    */
+    public void computeChouFasman(){
         // conformation states of all amino acids
         HashMap<Character, ArrayList> cs = new HashMap<Character, ArrayList>();
         // relative frequencies of amino acids
@@ -99,14 +114,14 @@ public class Data {
 
 
         for (DataItem di: this.data){
-            for (int i = 0; i < di.ssp_seq.length(); i++) {
-                if ("ZBJX".indexOf(di.aa_seq.charAt(i)) == -1)
+            for (int i = 0; i < di.length(); i++) {
+                if ("ZBJX".indexOf(di.getAaAt(i)) == -1)
                     continue;
 
-                int count = aa_counts.get(di.aa_seq.charAt(i));
-                aa_counts.put(di.aa_seq.charAt(i), count + 1);
+                int count = aa_counts.get(di.getAaAt(i));
+                aa_counts.put(di.getAaAt(i), count + 1);
 
-                if (di.ssp_seq.charAt(i) == 'H'){
+                if (di.getSspAt(i) == 'H'){
                     //count = cs.get(di.aa_seq.charAt(i)).get(0).intValue();
                     //cs.get(di.aa_seq.charAt(i)).set(0, count + 1);
                 }
@@ -115,10 +130,18 @@ public class Data {
     }
 
 
-    public void loadChouFasman(String file_path){
+    /**
+    * Loads Chou-Fasman parameters from data file in format:
+    * aa1 pHelix pSheet pCoil
+    * aa2 pHelix pSheet pCoil
+    * ...
+    *
+    * @param filePath path to file containing Chou-Fasman parameters for all amino acids
+    */
+    public void loadChouFasman(String filePath){
 
         try{
-            FileInputStream fstream = new FileInputStream(file_path);
+            FileInputStream fstream = new FileInputStream(filePath);
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
@@ -138,7 +161,7 @@ public class Data {
                     amino.cf_b = Integer.parseInt(match.group(3));
                     amino.cf_c = Integer.parseInt(match.group(4));
 
-                    this.amino_acids.put(new Character(amino.abbrev), amino);
+                    this.aminoAcids.put(new Character(amino.abbrev), amino);
                 }
             }
 
@@ -149,27 +172,156 @@ public class Data {
     }
 
 
-
+    /**
+    * Computes Q3 accuracy measure for actual Data object.
+    * Q3 is ratio of a number of good predicted amino acids
+    * to a number of all amino acids.
+    */
     public double q3(){
-        int all_count = 0;
-        int ok_count = 0;
+        int allCount = 0;
+        int okCount = 0;
 
         for (DataItem di : this.data){
-            for (int i = 0; i <  di.ssp_seq.length(); i++) {
-                if (di.ssp_seq.charAt(i) == di.predicted_seq.charAt(i))
-                    ok_count++;
+            for (int i = 0; i <  di.length(); i++) {
+                if (di.getSspAt(i) == di.getPredAt(i))
+                    okCount++;
             }
-            all_count += di.ssp_seq.length();
+            allCount += di.getSspSeq().length();
         }
-
-        return (double) ok_count/all_count;
+        return (double) okCount/allCount*100;
     }
 
 
 
+    /**
+    * Computes SOV accuracy measure for actual Data object.
+    * SOV takes into account segments overlaps.
+    */
     public double sov(){
-        // TODO
-        return 0;
+
+        int counter = 0;
+        char motiv;
+
+        Map<SOVSegment, ArrayList<SOVSegment>> olSegments;
+        ArrayList<SOVSegment> origSegments = new ArrayList<SOVSegment>();
+        ArrayList<SOVSegment> predSegments = new ArrayList<SOVSegment>();
+
+        for (int i = 0; i < this.data.size(); i++) {
+            counter++;
+            origSegments.addAll(getAllSegments(this.data.get(i).getSspSeq(), counter));
+            predSegments.addAll(getAllSegments(this.data.get(i).getPredSeq(), counter));
+        }
+
+        olSegments = getOverlappingSegments(origSegments, predSegments);
+
+        double n = computeNormConstant(olSegments);
+
+        double sumSOV = 0.0;
+
+        for (Map.Entry<SOVSegment, ArrayList<SOVSegment>> entry: olSegments.entrySet()) {
+            SOVSegment origSeg = entry.getKey();
+            ArrayList<SOVSegment> predSegs = entry.getValue();
+
+            if (predSegs.size() > 0){
+                for (SOVSegment predSeg: predSegs){
+
+                    double partSOV = SOVSegment.minov(origSeg, predSeg);
+                    partSOV += SOVSegment.delta(origSeg, predSeg);
+                    partSOV = partSOV / SOVSegment.maxov(origSeg, predSeg) * origSeg.length();
+                    sumSOV += partSOV;
+                }
+            }
+        }
+        return sumSOV/n*100;
+    }
+
+
+    /**
+    * Computes normalization constant needed for SOV accuracy measure computing.
+    *
+    * @param olSegments Map of overlapped segments, it is mapping original (not predicted)
+    * segment to all predicted segments it overlaps with.
+    */
+    private double computeNormConstant(Map<SOVSegment, ArrayList<SOVSegment>> olSegments){
+
+        // sum of lengths of overlapping segments
+        int sumOL = 0;
+
+        // sum of lengths of non overlapping segments
+        int sumNOL = 0;
+
+        for (Map.Entry<SOVSegment, ArrayList<SOVSegment>> entry : olSegments.entrySet()) {
+            SOVSegment origSeg = entry.getKey();
+            ArrayList<SOVSegment> predSegs = entry.getValue();
+
+            if (predSegs.size() == 0)
+                sumNOL += origSeg.length();
+            else
+                sumOL += origSeg.length() * predSegs.size();
+
+        }
+        return sumOL + sumNOL;
+    }
+
+
+    /**
+    *
+    */
+    private Map<SOVSegment, ArrayList<SOVSegment>> getOverlappingSegments(
+        ArrayList<SOVSegment> origSegments, ArrayList<SOVSegment> predSegments){
+
+        Map<SOVSegment, ArrayList<SOVSegment>> olSegments = new HashMap<SOVSegment, ArrayList<SOVSegment>>();
+
+
+        for (SOVSegment orig: origSegments){
+            olSegments.put(orig, new ArrayList<SOVSegment>());
+
+            for (SOVSegment pred: predSegments){
+                if (orig.isOverlapping(pred))
+                    olSegments.get(orig).add(pred);
+            }
+        }
+
+        return olSegments;
+    }
+
+
+
+
+
+    private ArrayList<SOVSegment> getAllSegments(String seq, int counter){
+
+        int start = 0;
+        int stop = 0;
+        boolean chainFlag = false;
+        char motiv = 'H';
+        ArrayList<SOVSegment> segments = new ArrayList<SOVSegment>();
+
+        for (int i = 0; i < seq.length(); i++) {
+            if (chainFlag == false){
+                start = i;
+                stop = i;
+                motiv = seq.charAt(i);
+                chainFlag = true;
+            }
+            else{
+                if (seq.charAt(i) != motiv){
+                    SOVSegment segment = new SOVSegment(counter, start, stop, motiv);
+                    segments.add(segment);
+                    start = i;
+                    stop = i;
+                    motiv = seq.charAt(i);
+                }
+                else{
+                    stop += 1;
+                }
+            }
+            if (i == seq.length() - 1){
+                SOVSegment segment = new SOVSegment(counter, start, stop, motiv);
+                segments.add(segment);
+            }
+        }
+        return segments;
     }
 
 
@@ -186,7 +338,29 @@ public class Data {
     }
 
     public void merge(Data data){
-        this.amino_acids = data.amino_acids;
+        this.aminoAcids = data.getAminoAcids();
         this.data.addAll(data.data);
+    }
+
+
+    /* Getters & setters */
+    public ArrayList<DataItem> getData(){
+        return this.data;
+    }
+
+    public void setData(ArrayList<DataItem> data){
+        this.data = data;
+    }
+
+    public void setAminoAcids(HashMap<Character, AminoAcid> aminoAcids){
+        this.aminoAcids = aminoAcids;
+    }
+
+    public AminoAcid getAminoAcid(char aa){
+        return this.aminoAcids.get(aa);
+    }
+
+    public HashMap<Character, AminoAcid> getAminoAcids(){
+        return this.aminoAcids;
     }
 }
---
+++ CASSP/src/cassp/data/SOVSegment.java
@@ -0,0 +1,79 @@
+/**
+*   SOVSegment.java
+*
+*   Copyright (c) 2013 Vladimir Brigant
+*   This software is distributed under the terms of the GNU General Public License.
+*/
+
+
+package cassp.data;
+
+
+import java.lang.Math;
+
+
+
+public class SOVSegment {
+
+    /**
+    * The length of intersection of segments s1 and s2.
+    */
+    static int minov(SOVSegment s1, SOVSegment s2){
+        int minov = 0;
+        if (s1.start > s2.start)
+            minov = Math.min(s1.stop, s2.stop) - s1.start + 1;
+        else
+            minov = Math.min(s1.stop, s2.stop) - s2.start + 1;
+        return minov;
+    }
+
+    /**
+    * The length of union of segments s1 and s2.
+    */
+    static int maxov(SOVSegment s1, SOVSegment s2){
+        int maxov = Math.max(s1.stop, s2.stop) - Math.min(s1.start, s2.start) + 1;
+        return maxov;
+    }
+
+    /**
+    * Minimum from: maxov - minov, minov, len(s1)/2, len(s2)/2
+    */
+    static int delta(SOVSegment s1, SOVSegment s2){
+
+        int min1 = Math.min(SOVSegment.maxov(s1,s2) - SOVSegment.minov(s1,s2), SOVSegment.minov(s1,s2));
+        int min2 = Math.min(s1.length()/2, s2.length()/2);
+
+        return Math.min(min1, min2);
+    }
+
+    public int seqId;
+    public int start;
+    public int stop;
+    public char motiv;
+
+    public SOVSegment(int seqId, int start, int stop, char motiv){
+        this.seqId = seqId;
+        this.start = start;
+        this.stop = stop;
+        this.motiv = motiv;
+    }
+
+    public boolean isOverlapping(SOVSegment pred){
+        // orig - this, pred - seg
+        if (((pred.start <= this.stop && pred.start >= this.start)
+            || (pred.stop >= this.start && pred.stop <= this.stop))
+            && this.seqId == pred.seqId && this.motiv == pred.motiv)
+            return true;
+        else
+            return false;
+    }
+
+    public int length(){
+        return (this.stop - this.start + 1);
+    }
+
+
+    public String toString(){
+        return this.seqId + ": " + this.start + ": " + this.stop + ": " + this.motiv;
+    }
+}
